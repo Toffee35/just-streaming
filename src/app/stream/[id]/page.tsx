6dbaaa045bb1485ab5963fea1c22125a331487a1
QuaternionWithSoul <nailzagru@gmail.com>
2025-01-06 19:21:03 +0300

    fix

--- src/app/stream/[id]/page.tsx
+++ src/app/stream/[id]/page.tsx
@@ -34,3 +34,9 @@ export async function generateMetadata({
     title: `Стрим ${id}`,
   }
 }
+
+export async function generateStaticParams() {
+  const ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17, 18, 19, 20];
+
+  return ids.map((id) => ({ id }));
+}
