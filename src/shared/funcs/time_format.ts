05f74fb6afdabdadd83baf04eab589a9bb5b6897
toffee35 <nailzagru@gmail.com>
2025-01-19 01:37:25 +0300

    Update time_format.ts

--- src/shared/funcs/time_format.ts
+++ src/shared/funcs/time_format.ts
@@ -2,10 +2,10 @@ export default function timeFormat(mins: number): string {
     if (mins < 1) {
         return "now"
     } else if (mins < 60) {
-        return `${mins} mins ago`
+        return `${mins}m ago`
     } else if (mins < 60 * 24) {
-        return `${Math.floor(mins / 60)} hours ago`
+        return `${Math.floor(mins / 60)}h ago`
     } else {
-        return `${Math.floor(mins / 60 / 24)} days ago`
+        return `${Math.floor(mins / 60 / 24)}d ago`
     }
 }
